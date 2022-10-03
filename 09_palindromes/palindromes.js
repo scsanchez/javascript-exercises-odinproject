0d7361b559fcd6ebcccbb400db1e2753a01cfe7e
Sergio Carrascal <sergiocarrascal000@gmail.com>
2022-10-03 19:54:24 +0200

    palindromes almost working without refactor

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let splitString = string.split("");
+  let reverseArray = splitString.reverse();
+  let joinArray = reverseArray.join("");
+  let newArray = joinArray
+    .toLowerCase()
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+  let splitString2 = newArray.split("");
+  let reverseArray2 = splitString2.reverse();
+  let joinArray2 = reverseArray2.join("");
+  return (
+    string.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "") ==
+    joinArray2
+  );
 };
 
 // Do not edit below this line
